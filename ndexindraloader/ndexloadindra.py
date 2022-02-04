1e62a2a327787c6e2c7ea43bf047091f52f839b4
Chris Churas <churas.camera@gmail.com>
2022-02-04 13:20:25 -0800

    updated code to apply style kept with package by default via --style flag

--- ndexindraloader/ndexloadindra.py
+++ ndexindraloader/ndexloadindra.py
@@ -106,6 +106,8 @@ def _parse_arguments(desc, args):
                         help='If set, adds source edge attribute with value'
                              'passed in to existing edges of network')
     parser.add_argument('--style',
+                        default=os.path.join(os.path.dirname(ndexindraloader.indra.__file__),
+                                             'style.cx'),
                         help='If CX file, then style from that file is '
                              'applied to network.')
     parser.add_argument('--tmpdir', default='.',
